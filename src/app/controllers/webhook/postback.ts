6a183857e95b9cb37bb8ca75cc53418d8792b6fa
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-08-21 06:21:41 +0000

    # CINERINO-1 ### Added - line-bot-sdkで再構築。

--- src/app/controllers/webhook/postback.ts
+++ src/app/controllers/webhook/postback.ts
@@ -227,7 +227,7 @@ export async function askScreeningEvent(params: {
     });
     const startFrom = moment(Math.max(moment(`${params.date}T00:00:00+09:00`).unix(), moment().unix())).toDate();
     const startThrough = moment(`${params.date}T00:00:00+09:00`).add(1, 'day').toDate();
-    await LINE.replyMessage(params.replyToken, { type: 'text', text: `${startFrom}-${startThrough}のイベントを検索しています...` });
+    await LINE.pushMessage(params.replyToken, { type: 'text', text: `${startFrom}-${startThrough}のイベントを検索しています...` });
     let screeningEvents = await eventService.searchScreeningEvents({
         startFrom: startFrom,
         startThrough: startThrough
